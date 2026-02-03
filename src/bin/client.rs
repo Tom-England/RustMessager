3cc5b42be86cf69f348f6ce6c133e6cee48c54aa
Tom-England <tomengland6679@gmail.com>
2026-02-03 21:02:23 +0000

    Minor Error handling and more graceful exit for server

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -2,11 +2,35 @@ use tokio::io::{self, AsyncWriteExt};
 use tokio::net::{TcpStream};
 use std::error::Error;
 
-#[tokio::main]
-pub async fn main() -> Result<(), Box<dyn Error>> {
+
+async fn run_client() {
     let mut stream = TcpStream::connect("127.0.0.1:80").await.unwrap();
+	loop {
+		stream.writable().await.unwrap();
+    	stream.write_all(b"Hello, World!").await.unwrap();
+	}
+}
 
-    stream.write_all(b"Hello, World!").await?;
+fn write_value(stream: &TcpStream) {
+	match stream.try_write(b"Hello, World!") {
+		Ok(_n) => {
+			;
+		},
+		Err(ref e) if e.kind()  == io::ErrorKind::WouldBlock => {
+			println!("Block More n00b");
+		},
+		Err(e) => {
+			eprintln!("{:?}",e);
+		}
+	}
+}
 
-    Ok(())
+#[tokio::main]
+pub async fn main() {
+
+    let mut stream = TcpStream::connect("127.0.0.1:80").await.unwrap();
+	loop {
+		stream.writable().await.unwrap();
+    	write_value(&stream);
+	}
 } // the stream is closed here
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,23 +1,24 @@
 use tokio::io::{AsyncReadExt};
 use tokio::net::{TcpListener, TcpStream};
 use std::error::Error;
+use tokio::signal;
 
 async fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>>
 {
-	stream.readable().await?;
-	let mut buffer: [u8; 128] = [0; 128]; 
-	let read_bytes = stream.try_read(&mut buffer[..]);
+	loop {
+		stream.readable().await?;
+		let mut buffer: [u8; 128] = [0; 128]; 
+		let read_bytes = stream.try_read(&mut buffer[..]);
 
-	let output: String = String::from_utf8(buffer[..read_bytes.unwrap()].to_vec())?;
+		let output: String = String::from_utf8(buffer[..read_bytes.unwrap()].to_vec())?;
 
-	println!("Received: {}",output);
-
+		println!("Received: {}",output);
+	}
 	Ok(())
 }
 
-#[tokio::main]
-pub async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:80").await.unwrap();
+async fn run_server() {
+	let listener = TcpListener::bind("127.0.0.1:80").await.unwrap();
 
 	loop {
         // The second item contains the IP and port of the new connection.
@@ -26,5 +27,22 @@ pub async fn main() -> std::io::Result<()> {
             handle_client(socket).await;
         });
     }
+}
+
+#[tokio::main]
+pub async fn main() -> std::io::Result<()> {
+    tokio::spawn(async move {
+        run_server().await;
+    });
+	
+	match signal::ctrl_c().await {
+        Ok(()) => {},
+        Err(err) => {
+            eprintln!("Unable to listen for shutdown signal: {}", err);
+            // we also shut down in case of error
+        },
+    }
+	
+
     Ok(())
 }
